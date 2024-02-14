cda3b6c4beccea40870801061cb1a4e50da998b6
nxitik <nrzaveri@wpi.edu>
2024-02-14 11:56:45 -0500

    Committing Validation stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,3 +12,12 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f"Starting {STAGE_NAME}.... started")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f"Completed {STAGE_NAME}.... completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
