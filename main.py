9ed3f184be2c441a7e265f75e388aa57b05d0827
nxitik <nrzaveri@wpi.edu>
2024-02-18 22:52:30 -0500

    Model trainer stage complete

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f"Starting {STAGE_NAME}.... started")
@@ -26,6 +27,16 @@ STAGE_NAME = "Data Transformation stage"
 try:
     logger.info(f"Starting {STAGE_NAME}.... started")
     obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f"Completed {STAGE_NAME}.... completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f"Starting {STAGE_NAME}.... started")
+    obj = ModelTrainerTrainingPipeline()
     obj.main()
     logger.info(f"Completed {STAGE_NAME}.... completed")
 except Exception as e:
